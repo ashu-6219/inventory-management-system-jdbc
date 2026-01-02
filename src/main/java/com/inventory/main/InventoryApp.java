854e7ba5b583f140cfe9573535a5da2bfdbfe646
Ashutosh Gajankush <ashutoshgajankush6219@gmail.com>
2026-01-02 11:14:41 +0530

    Added InventoryApp menu for testing inventory operations

---
+++ src/main/java/com/inventory/main/InventoryApp.java
@@ -0,0 +1,92 @@
+package com.inventory.main;
+import com.inventory.model.*;
+import com.inventory.dao.*;
+import java.util.*;
+import com.inventory.service.InventoryService;
+public class InventoryApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ProductDAO productDAO = new ProductDAO();
+        InventoryService inventoryService = new InventoryService();
+
+        while (true){
+            System.out.println("\n===== INVENTORY MANAGEMENT SYSTEM =====");
+            System.out.println("1. Add Product");
+            System.out.println("2. View All Products");
+            System.out.println("3. Stock IN");
+            System.out.println("4. Stock OUT");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = sc.nextInt();
+
+            switch (choice){
+                case 1: sc.nextLine();
+                    System.out.print("Enter Product Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter category: ");
+                    String category = sc.nextLine();
+
+                    System.out.print("Enter price: ");
+                    double price = sc.nextDouble();
+
+                    System.out.print("Enter initial quantity: ");
+                    int qty = sc.nextInt();
+
+                    Product product = new Product(name, category, price, qty);
+                    productDAO.addProduct(product);
+                    break;
+
+
+                case 2:
+                    List<Product> products = productDAO.getAllProducts();
+                    System.out.println("\n--- PRODUCT LIST ---");
+                    for (Product p : products) {
+                        System.out.println(
+                                p.getId() + " | " +
+                                        p.getName() + " | " +
+                                        p.getCategory() + " | " +
+                                        p.getPrice() + " | Qty: " +
+                                        p.getQuantity()
+                        );
+                    }
+                    break;
+
+
+                case 3:
+                    System.out.print("Enter product ID: ");
+                    int inId = sc.nextInt();
+
+                    System.out.print("Enter quantity to add: ");
+                    int inQty = sc.nextInt();
+
+                    inventoryService.processTransaction(
+                            new StockTransaction(inId, "IN", inQty)
+                    );
+                    break;
+
+
+                case 4:
+                    System.out.print("Enter product ID: ");
+                    int outId = sc.nextInt();
+
+                    System.out.print("Enter quantity to remove: ");
+                    int outQty = sc.nextInt();
+
+                    inventoryService.processTransaction(
+                            new StockTransaction(outId, "OUT", outQty)
+                    );
+                    break;
+
+                case 5:
+                    System.out.println("Exiting Inventory System...");
+                    sc.close();
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
+    }
+}
